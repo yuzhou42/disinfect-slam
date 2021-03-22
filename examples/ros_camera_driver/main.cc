65eecfae438c89934ef1a8a4cfc0bd2a8afad722
yuzhou42 <yu.nico.zhou@gmail.com>
2021-03-22 12:10:08 -0500

    add mesh reconstruction

--- examples/ros_camera_driver/main.cc
+++ examples/ros_camera_driver/main.cc
@@ -26,6 +26,28 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <image_transport/image_transport.h>
 
+#include <KrisLibrary/geometry/TSDFReconstruction.h>
+#include <KrisLibrary/math3d/AABB3D.h>
+#include <KrisLibrary/meshing/IO.h>
+#include <KrisLibrary/meshing/TriMeshOperators.h>
+#include <KrisLibrary/utils/ioutils.h>
+#include <KrisLibrary/utils.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <ros/ros.h>
+#include <shape_msgs/Mesh.h>
+#include <geometry_msgs/Point.h>
+#include <shape_msgs/MeshTriangle.h>
+#include <rviz_visual_tools/rviz_visual_tools.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2/convert.h>
+#include <chrono>
+#include <cinttypes>
+
+#define CELL_SIZE 0.05
+#define TRUNCATION_DISTANCE -0.1
 
 using namespace std;
 // std::mutex mtx;   
@@ -33,6 +55,14 @@ cv::Mat zedLeftMask;
 cv::Mat l515Mask;
 std::mutex mask_lock;
 std::mutex zed_mask_lock;
+
+rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
+tf2_ros::Buffer tfBuffer;
+ros::Publisher meshPub;
+std::shared_ptr<tf2_ros::TransformListener> tfListener;
+std_msgs::Float32MultiArray::Ptr mReconstrMsg;
+geometry_msgs::TransformStamped transformStamped;
+
 
 
 void publishImage(cv_bridge::CvImagePtr & imgBrgPtr, const cv::Mat & img, ros::Publisher & pubImg, std::string imgFrameId, std::string dataType, ros::Time t){
@@ -43,20 +73,117 @@ void publishImage(cv_bridge::CvImagePtr & imgBrgPtr, const cv::Mat & img, ros::P
   pubImg.publish(imgBrgPtr->toImageMsg());
 }
 
+void tsdfCb(const std_msgs::Float32MultiArray::Ptr& msg)
+{
+    static bool init = false;
+    static Eigen::Isometry3d pose;
+    static geometry_msgs::TransformStamped transformStamped;
+    if(!init){
+        try{
+        transformStamped = tfBuffer.lookupTransform("world", "slam",
+                                ros::Time(0));
+        pose = tf2::transformToEigen(transformStamped);
 
-void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSystem> my_sys,  ros::NodeHandle& mNh, tf2_ros::TransformBroadcaster& mTfSlam, bool renderFlag, double   bbox_xy) {
+        }
+        catch (tf2::TransformException &ex) {
+        ROS_WARN("%s",ex.what());
+        }
+        init = true;
+    }
+
+    auto values = msg->data;
+    // ROS_INFO("I heard tsdf of size: ", msg->data.size());
+    const auto st = GetTimestamp<std::chrono::milliseconds>();  // nsec
+
+    int numPoints = msg->data.size()/4;
+    float minValue = 1e100, maxValue = -1e100;
+    Math3D::AABB3D bbox;
+    int k=0;
+    for(int i=0;i<numPoints;i++,k+=4) {
+        minValue = Min(minValue,values[k+3]);
+        maxValue = Max(maxValue,values[k+3]);
+        bbox.expand(Math3D::Vector3(values[k],values[k+1],values[k+2]));
+    }
+    // printf("Read %d points with distance in range [%g,%g]\n",numPoints,minValue,maxValue);
+    // printf("   x range [%g,%g]\n",bbox.bmin.x,bbox.bmax.x);
+    // printf("   y range [%g,%g]\n",bbox.bmin.y,bbox.bmax.y);
+    // printf("   z range [%g,%g]\n",bbox.bmin.z,bbox.bmax.z);
+    float truncation_distance = TRUNCATION_DISTANCE;
+    if(TRUNCATION_DISTANCE < 0) {
+        //auto-detect truncation distance
+        truncation_distance = Max(-minValue,maxValue)*0.99;
+        printf("Auto-detected truncation distance %g\n",truncation_distance);
+    }
+    // printf("Using cell size %g\n",CELL_SIZE);
+    Geometry::SparseTSDFReconstruction tsdf(Math3D::Vector3(CELL_SIZE),truncation_distance);
+    tsdf.tsdf.defaultValue[0] = truncation_distance;
+    k=0;
+    Math3D::Vector3 ofs(CELL_SIZE*0.5);
+    for(int i=0;i<numPoints;i++,k+=4) {
+        tsdf.tsdf.SetValue(Math3D::Vector3(values[k],values[k+1],values[k+2])+ofs,values[k+3]);
+    }
+
+    // printf("Extracting mesh\n");
+    Meshing::TriMesh mesh;
+    tsdf.ExtractMesh(mesh);
+    // std::cout<<"Before Merge: vertsSize: "<<mesh.verts.size()<<std::endl;
+    // std::cout<<"Before Merge: trisSize: "<<mesh.tris.size()<<std::endl;
+
+    MergeVertices(mesh, 0.05);
+
+    int vertsSize = mesh.verts.size();
+    int trisSize = mesh.tris.size();
+
+    std::cout<<"vertsSize: "<<vertsSize<<std::endl;
+    std::cout<<"trisSize: "<<trisSize<<std::endl;
+    const auto end = GetTimestamp<std::chrono::milliseconds>();
+    std::cout<<"mesh processing time: "<<end-st<<" ms"<<std::endl;
+
+    const auto st_msg = GetTimestamp<std::chrono::milliseconds>();  
+    shape_msgs::Mesh::Ptr mMeshMsg = boost::make_shared<shape_msgs::Mesh>();
+    // geometry_msgs/Point[] 
+    mMeshMsg->vertices.resize(vertsSize);
+
+    // // // shape_msgs/MeshTriangle[] 
+    mMeshMsg->triangles.resize(trisSize);
+    
+
+    for(int i = 0; i < vertsSize; i++){
+        mMeshMsg->vertices[i].x = mesh.verts[i].x;
+        mMeshMsg->vertices[i].y = mesh.verts[i].y;
+        mMeshMsg->vertices[i].z = mesh.verts[i].z;
+        // std::cout<<mesh.verts[i].x<<std::endl;
+    }
+
+    for(int i = 0; i < trisSize; i++){
+        mMeshMsg->triangles[i].vertex_indices[0] = mesh.tris[i].a;
+        mMeshMsg->triangles[i].vertex_indices[1] = mesh.tris[i].b;
+        mMeshMsg->triangles[i].vertex_indices[2] = mesh.tris[i].c;
+        // std::cout<<mesh.tris[i].a<<std::endl;
+    }
+    meshPub.publish(mMeshMsg);
+    // Eigen::Isometry3d pose;
+    // pose = Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY()); // rotate along X axis by 45 degrees
+    // pose.translation() = Eigen::Vector3d( 0, 0, 0 ); // translate x,y,z
+    // Publish arrow vector of pose
+    visual_tools_->publishMesh(pose, *mMeshMsg, rviz_visual_tools::ORANGE, 1, "mesh", 1); // rviz_visual_tools::TRANSLUCENT_LIGHT
+
+    const auto end_msg = GetTimestamp<std::chrono::milliseconds>();  
+    // Don't forget to trigger the publisher!
+    visual_tools_->trigger();
+}
+
+void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSystem> my_sys,  ros::NodeHandle& mNh, tf2_ros::TransformBroadcaster& mTfSlam, bool renderFlag, double   bbox_xy, bool global_mesh) {
   SE3<float> mSlamPose;
   std::vector<VoxelSpatialTSDF>  mSemanticReconstr;
-  std_msgs::Float32MultiArray::Ptr mReconstrMsg(new std_msgs::Float32MultiArray);
-  geometry_msgs::TransformStamped transformStamped;
 
   // publishers
   ros::Publisher mPubL515RGB = mNh.advertise<sensor_msgs::Image>("/l515_rgb", 1);
   ros::Publisher mPubL515Depth = mNh.advertise<sensor_msgs::Image>("/l515_depth", 1);
   ros::Publisher mPubZEDImgL = mNh.advertise<sensor_msgs::Image>("/zed_left_rgb", 1);
   ros::Publisher mPubZEDImgR = mNh.advertise<sensor_msgs::Image>("/zed_right_rgb", 1);
-  ros::Publisher mPubTsdfGlobal = mNh.advertise<std_msgs::Float32MultiArray>("/tsdf_global", 4);
-  ros::Publisher mPubTsdfLocal = mNh.advertise<std_msgs::Float32MultiArray>("/tsdf_local", 4);
+  // ros::Publisher mPubTsdfGlobal = mNh.advertise<std_msgs::Float32MultiArray>("/tsdf_global", 4);
+  // ros::Publisher mPubTsdfLocal = mNh.advertise<std_msgs::Float32MultiArray>("/tsdf_local", 4);
 
   // cv bridges
   cv_bridge::CvImagePtr mL515RGBBrg;
@@ -74,7 +201,7 @@ void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSy
     cv::Mat img_left, img_right, zedLeftMaskL;
     ros::Time ros_stamp;
     while (ros::ok()) {
-      const int64_t timestamp = zed_native.get_stereo_img(&img_left, &img_right);
+      const int64_t timestamp = zed_native.GetStereoFrame(&img_left, &img_right);
       zed_mask_lock.lock();
       zedLeftMaskL = zedLeftMask.clone();
       zed_mask_lock.unlock();
@@ -95,8 +222,7 @@ void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSy
     cv::Mat img_rgb, img_depth, l515MaskL;
     ros::Time ros_stamp;
     while (ros::ok()) {
-      const int64_t timestamp = l515.get_rgbd_frame(&img_rgb, &img_depth);
-        std::cout << "TSDF Thread: " << &mask_lock << std::endl;
+      const int64_t timestamp = l515.GetRGBDFrame(&img_rgb, &img_depth);
         mask_lock.lock();
         l515MaskL = l515Mask.clone();
         // cv::imshow("mask_depth", l515MaskL);
@@ -131,40 +257,36 @@ void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSy
       ros::Rate rate(4);
 
     while (ros::ok()) {
-      uint32_t tsdf_global_sub = mPubTsdfGlobal.getNumSubscribers();
-      uint32_t tsdf_local_sub = mPubTsdfLocal.getNumSubscribers();
-
-      if(tsdf_global_sub > 0){
-        const auto st = get_timestamp<std::chrono::milliseconds>();  // nsec
-        auto mSemanticReconstr = my_sys->query_tsdf(volumn);
-        const auto end = get_timestamp<std::chrono::milliseconds>();
-        last_query_time = end - st;
-        last_query_amount = mSemanticReconstr.size();
-        std::cout<<"Last queried %lu voxels "<<last_query_amount<<", took "<< last_query_time<<" ms"<<std::endl;
-        int size = last_query_amount * sizeof(VoxelSpatialTSDF);
-        mReconstrMsg->data.resize(size);
-        std::memcpy(&(mReconstrMsg->data[0]), (char*)mSemanticReconstr.data(),  last_query_amount * sizeof(VoxelSpatialTSDF));
-        mPubTsdfGlobal.publish(mReconstrMsg);
+        if (!global_mesh)
+      {
+          const auto st = GetTimestamp<std::chrono::milliseconds>(); // nsec
+          float x_off   = transformStamped.transform.translation.x,
+                y_off   = transformStamped.transform.translation.y,
+                z_off   = transformStamped.transform.translation.z;
+          std::cout << "x_off: " << x_off << "  y_off: " << y_off << "  z_off: " << z_off
+                    << std::endl;
+          volumn = {x_off + x_range[0],
+                      x_off + x_range[1],
+                      y_off + y_range[0],
+                      y_off + y_range[1],
+                      z_off + z_range[0],
+                      z_off + z_range[1]};
       }
 
-      if(tsdf_local_sub > 0){
-        const auto st = get_timestamp<std::chrono::milliseconds>();  // nsec
-        float x_off = transformStamped.transform.translation.x, y_off = transformStamped.transform.translation.y, z_off = transformStamped.transform.translation.z;
-        std::cout<<"x_off: "<<x_off<<"  y_off: "<<y_off<<"  z_off: "<<z_off<<std::endl; 
-        BoundingCube<float> volumn_local = {
-        x_off + x_range[0], x_off+ x_range[1], y_off + y_range[0], y_off + y_range[1], z_off + z_range[0], z_off + z_range[1]};
-        auto mSemanticReconstr = my_sys->query_tsdf(volumn_local);
-        const auto end = get_timestamp<std::chrono::milliseconds>();
-        last_query_time = end - st;
-        last_query_amount = mSemanticReconstr.size();
-        std::cout<<"Last queried %lu voxels "<<last_query_amount<<", took "<< last_query_time<<" ms"<<std::endl;
-        int size = last_query_amount * sizeof(VoxelSpatialTSDF);
-        mReconstrMsg->data.resize(size);
-        std::memcpy(&(mReconstrMsg->data[0]), (char*)mSemanticReconstr.data(),  last_query_amount * sizeof(VoxelSpatialTSDF));
-        mPubTsdfLocal.publish(mReconstrMsg);
-      }
+      const auto st          = GetTimestamp<std::chrono::milliseconds>(); // nsec
+      auto mSemanticReconstr           = my_sys->query_tsdf(volumn);
+      const auto end                   = GetTimestamp<std::chrono::milliseconds>();
+      last_query_time                  = end - st;
+      last_query_amount                = mSemanticReconstr.size();
+      std::cout << "Last queried %lu voxels " << last_query_amount << ", took " << last_query_time
+                  << " ms" << std::endl;
+      int size = last_query_amount * sizeof(VoxelSpatialTSDF);
+      mReconstrMsg->data.resize(size);
+      std::memcpy(&(mReconstrMsg->data[0]),
+                  (char*)mSemanticReconstr.data(),
+                  last_query_amount * sizeof(VoxelSpatialTSDF));
 
-
+      tsdfCb(mReconstrMsg);
       ros::spinOnce();
       rate.sleep();
     }
@@ -178,7 +300,7 @@ void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSy
       // u_int64_t tNow = std::chrono::steady_clock::now().time_since_epoch().count();  // nsec
       // stamp.sec += tNow / 1000000000UL; //s
       // stamp.nsec = tNow % 1000000000UL; //ns
-      u_int64_t t_query = get_timestamp<std::chrono::milliseconds>();
+      u_int64_t t_query = GetTimestamp<std::chrono::milliseconds>();
       stamp.sec  = t_query / 1000; //s
       stamp.nsec = (t_query % 1000) * 1000 * 1000; //ns
       mSlamPose = my_sys->query_camera_pose(t_query);
@@ -186,13 +308,11 @@ void run(const ZEDNative &zed_native, const L515 &l515, std::shared_ptr<DISINFSy
       tf2::Transform tf2_trans;
       tf2::Transform tf2_trans_inv;
 
-      tf2::Matrix3x3 R(mSlamPose.m00,mSlamPose.m01,mSlamPose.m02,
-                          mSlamPose.m10,mSlamPose.m11,mSlamPose.m12,
-                          mSlamPose.m20,mSlamPose.m21,mSlamPose.m22);
-      tf2::Vector3 T(mSlamPose.m03, mSlamPose.m13, mSlamPose.m23);
+      Eigen::Quaternion<float> R = mSlamPose.GetR();
+      Eigen::Matrix<float, 3, 1> T = mSlamPose.GetT();
 
-      tf2_trans.setBasis(R);
-      tf2_trans.setOrigin(T);
+      tf2_trans.setRotation(tf2::Quaternion(R.x(), R.y(), R.z(), R.w()));
+      tf2_trans.setOrigin(tf2::Vector3(T.x(), T.y(), T.z()));
 
       tf2_trans_inv = tf2_trans.inverse();
 
@@ -271,9 +391,18 @@ int main(int argc, char *argv[]) {
   image_transport::Subscriber sub2 = it.subscribe("/robot_mask/realsense_slam_l515", 1, l515MaskCb);
 
   tf2_ros::TransformBroadcaster mTfSlam;
+
+  visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("world","/mesh_visual", mNh));
+  visual_tools_->setPsychedelicMode(false);
+  visual_tools_->loadMarkerPub();
+  tfListener= std::make_shared<tf2_ros::TransformListener>(tfBuffer);
+  meshPub = mNh.advertise<shape_msgs::Mesh>("/mesh", 1);
+  mReconstrMsg.reset(new std_msgs::Float32MultiArray);
+
   std::string model_path, calib_path, orb_vocab_path;
   int devid;
   bool renderFlag;
+  bool global_mesh;
   double bbox_xy;
 
   mNh.getParam("/ros_disinf_slam/model_path", model_path); 
@@ -282,18 +411,20 @@ int main(int argc, char *argv[]) {
   mNh.param("/ros_disinf_slam/devid", devid, 2);
   mNh.param("/ros_disinf_slam/bbox_xy", bbox_xy, 4.0);
   mNh.param("/ros_disinf_slam/renderer", renderFlag, false);
+  mNh.param("/ros_disinf_slam/global_mesh", global_mesh, true);
+
   // ROS_INFO_STREAM(calib_path);
 
-  auto cfg = get_and_set_config(calib_path);
+  auto cfg = GetAndSetConfig(calib_path);
   // initialize cameras
   ZEDNative zed_native(*cfg, devid);
   L515 l515;
   // initialize slam
   std::shared_ptr<DISINFSystem> my_system = std::make_shared<DISINFSystem>(
-      calib_path,orb_vocab_path, model_path, renderFlag
+      calib_path, orb_vocab_path, model_path, renderFlag
   );
 
-  run(zed_native, l515, my_system, mNh, mTfSlam, renderFlag, bbox_xy);
+  run(zed_native, l515, my_system, mNh, mTfSlam, renderFlag, bbox_xy, global_mesh);
 
   return EXIT_SUCCESS;
 }
