10177096788bcb5fa3a343363a66ed61f6dbcf73
yuzhou42 <yu.nico.zhou@gmail.com>
2021-06-27 15:17:25 -0500

    obtain IMU data from zed

--- cameras/zed.cc
+++ cameras/zed.cc
@@ -1,12 +1,24 @@
 #include "zed.h"
+#define DEG2RAD 0.017453293
+#define RAD2DEG 57.295777937
 
 ZED::ZED() {
   sl::InitParameters init_params;
+  // No depth computation required here
+  init_params.depth_mode = sl::DEPTH_MODE::NONE;
   init_params.camera_resolution = sl::RESOLUTION::VGA;
   init_params.camera_fps = 30;
-  init_params.coordinate_units = sl::UNIT::MILLIMETER;
-  init_params.depth_mode = sl::DEPTH_MODE::QUALITY;
-  zed_.open(init_params);
+  init_params.coordinate_system = sl::COORDINATE_SYSTEM::RIGHT_HANDED_Z_UP_X_FWD;  // Yu: Important to set!!!
+  init_params.coordinate_units = sl::UNIT::METER;
+  init_params.enable_image_enhancement = true; // Always active
+
+  // init_params.depth_mode = sl::DEPTH_MODE::QUALITY;
+  // Open the camera
+  sl::ERROR_CODE err = zed_.open(init_params);
+  if (err != sl::ERROR_CODE::SUCCESS) {
+      std::cout << "Error " << err << ", exit program.\n";
+  }
+
   zed_.setCameraSettings(sl::VIDEO_SETTINGS::EXPOSURE, 100);
   rt_params_ = zed_.getRuntimeParameters();
   rt_params_.confidence_threshold = 50;
@@ -16,6 +28,83 @@ ZED::ZED() {
 ZED::~ZED() { zed_.close(); }
 
 sl::CameraConfiguration ZED::GetConfig() const { return config_; }
+
+// Todo: create a timer for obtaining IMU data
+void ZED::GetIMU(){
+  static int64_t tz_prev_imu = 0;
+  static int cnt = 0;
+  static int64_t total_t = 0;
+  if(zed_.getSensorsData(sensors_data_, sl::TIME_REFERENCE::CURRENT) != sl::ERROR_CODE::SUCCESS ) {
+            std::cout<<"Not retrieved sensors data in CURRENT REFERENCE TIME"<<std::endl;
+            return;
+        }
+  //Incoming packets are time stamped at reception by the host machine, in Epoch time and nanosecond resolution
+  int64_t tz_imu = sensors_data_.imu.timestamp;
+  if(tz_imu == tz_prev_imu) return;
+  cnt++;
+  total_t += (tz_imu - tz_prev_imu);
+  if (total_t>=1e9){
+    std::cout<<"freq of imu: "<<cnt<<std::endl; //405
+    cnt = 0;
+    total_t = 0;
+  }
+  tz_prev_imu = tz_imu;
+  cv::Vec4d q(sensors_data_.imu.pose.getOrientation()[0],
+              sensors_data_.imu.pose.getOrientation()[1],
+              sensors_data_.imu.pose.getOrientation()[2],
+              sensors_data_.imu.pose.getOrientation()[3]); // x,y,z,w
+  cv::Point3f gyr(sensors_data_.imu.angular_velocity[0] * DEG2RAD,sensors_data_.imu.angular_velocity[1] * DEG2RAD,sensors_data_.imu.angular_velocity[2] * DEG2RAD);
+  cv::Point3f acc(sensors_data_.imu.linear_acceleration[0],sensors_data_.imu.linear_acceleration[1],sensors_data_.imu.linear_acceleration[2]);
+  
+  simuData imuData;
+  imuData.w = gyr;
+  imuData.a = acc;
+  imuData.t = tz_imu;
+  imuData.q = q;
+  // std::cout<<"before lock in driver"<<std::endl;
+  // mBufMutex.lock();
+  qImuBuf.push(std::move(imuData)); //double check
+  // mBufMutex.lock();
+
+    // sensors_data.imu contains new data
+  // std::cout << "IMU Orientation: {" << sensors_data_.imu.pose.getOrientation() << "}"<<std::endl;
+  // std::cout << "IMU Linear Acceleration: {" << sensors_data_.imu.linear_acceleration << "} [m/sec^2]"<<std::endl;
+  // std::cout << "IMU Angular Velocity: {" << sensors_data_.imu.angular_velocity << "} [deg/sec]"<<std::endl;
+  // std::cout <<"IMU time: "<<tz_imu/1e9<<std::endl;
+}
+
+double ZED::GetStereoFrame(cv::Mat* left_img, cv::Mat* right_img) {
+  static int64_t tz_prev_img = 0;
+  static int cnt = 0;
+  static int64_t total_t = 0;
+  cnt++;
+  AllocateIfNeeded(left_img, CV_8UC1);
+  AllocateIfNeeded(right_img, CV_8UC1);
+  sl::Mat left_sl(config_.resolution, sl::MAT_TYPE::U8_C1, left_img->data,
+                  config_.resolution.width);
+  sl::Mat right_sl(config_.resolution, sl::MAT_TYPE::U8_C1, right_img->data,
+                   config_.resolution.width);
+
+  if (zed_.grab(rt_params_) == sl::ERROR_CODE::SUCCESS) {
+    zed_.retrieveImage(left_sl, sl::VIEW::LEFT_GRAY);
+    zed_.retrieveImage(right_sl, sl::VIEW::RIGHT_GRAY);
+  }
+  int64_t tz_img = zed_.getTimestamp(sl::TIME_REFERENCE::IMAGE); // Get image timestamp
+
+  //the system time is 20075950 nano seconds slower than the time from the sensor driver
+  // const auto ts_img = (int64_t)(GetSystemTimestamp<std::chrono::nanoseconds>());
+  // std::cout<<"time diff: "<<ts_img - tz_img<<std::endl;  
+
+  total_t += (tz_img - tz_prev_img);
+  if (total_t >= 1e9){
+    std::cout<<"freq of image: "<<cnt<<std::endl; //30
+    cnt = 0;
+    total_t = 0;
+  }
+  tz_prev_img = tz_img;
+  return tz_img;
+}
+
 
 void ZED::GetStereoAndRGBDFrame(cv::Mat* left_img, cv::Mat* right_img, cv::Mat* rgb_img,
                                 cv::Mat* depth_img) {
--- cameras/zed.h
+++ cameras/zed.h
@@ -1,11 +1,20 @@
 #pragma once
-
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 #include <sl/Camera.hpp>
-
+#include "utils/time.hpp"
+#include<queue>
+#include<thread>
+#include<mutex>
 /**
  * @brief ZED camera interface using ZED SDK
  */
+struct simuData{
+  cv::Vec4d q;
+  cv::Point3f a;
+  cv::Point3f w;
+  int64_t t;
+};
 class ZED {
  public:
   ZED();
@@ -24,13 +33,16 @@ class ZED {
    * @param rgb_img     rgb image of RGBD frame
    * @param depth_img   depth image of RGBD frame
    */
+  double GetStereoFrame(cv::Mat* left_img, cv::Mat* right_img);
   void GetStereoAndRGBDFrame(cv::Mat* left_img, cv::Mat* right_img, cv::Mat* rgb_img,
                              cv::Mat* depth_img);
-
- private:
-  void AllocateIfNeeded(cv::Mat* img, int type) const;
-
+  void GetIMU();
   sl::Camera zed_;
+  sl::SensorsData sensors_data_;
   sl::CameraConfiguration config_;
   sl::RuntimeParameters rt_params_;
+  std::queue<simuData> qImuBuf;  // IMU buffer 
+  std::mutex mBufMutex;
+ private:
+  void AllocateIfNeeded(cv::Mat* img, int type) const;
 };
