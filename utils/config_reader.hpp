07ddf491b6bafaa16f4727200b059b4af9fb437f
yuzhou42 <yu.nico.zhou@gmail.com>
2021-07-07 14:23:23 -0500

    remove openvslam dependency

--- utils/config_reader.hpp
+++ utils/config_reader.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <openvslam/publish/map_publisher.h>
-#include <openvslam/system.h>
+// #include <openvslam/publish/map_publisher.h>
+// #include <openvslam/system.h>
 #include <yaml-cpp/yaml.h>
 
 #include <string>
@@ -29,14 +29,14 @@ inline SE3<float> GetExtrinsicsFromFile(const std::string& config_file_path) {
   return SE3<float>(tmp);
 }
 
-inline std::shared_ptr<openvslam::config> GetAndSetConfig(const std::string& config_file_path) {
-  YAML::Node yaml_node = YAML::LoadFile(config_file_path);
-  const StereoRectifier rectifier(yaml_node);
-  const cv::Mat rectified_intrinsics = rectifier.RectifiedIntrinsics();
-  yaml_node["Camera.fx"] = rectified_intrinsics.at<double>(0, 0);
-  yaml_node["Camera.fy"] = rectified_intrinsics.at<double>(1, 1);
-  yaml_node["Camera.cx"] = rectified_intrinsics.at<double>(0, 2);
-  yaml_node["Camera.cy"] = rectified_intrinsics.at<double>(1, 2);
-  yaml_node["Camera.focal_x_baseline"] = -rectified_intrinsics.at<double>(0, 3);
-  return std::make_shared<openvslam::config>(yaml_node);
-}
+// inline std::shared_ptr<openvslam::config> GetAndSetConfig(const std::string& config_file_path) {
+//   YAML::Node yaml_node = YAML::LoadFile(config_file_path);
+//   const StereoRectifier rectifier(yaml_node);
+//   const cv::Mat rectified_intrinsics = rectifier.RectifiedIntrinsics();
+//   yaml_node["Camera.fx"] = rectified_intrinsics.at<double>(0, 0);
+//   yaml_node["Camera.fy"] = rectified_intrinsics.at<double>(1, 1);
+//   yaml_node["Camera.cx"] = rectified_intrinsics.at<double>(0, 2);
+//   yaml_node["Camera.cy"] = rectified_intrinsics.at<double>(1, 2);
+//   yaml_node["Camera.focal_x_baseline"] = -rectified_intrinsics.at<double>(0, 3);
+//   return std::make_shared<openvslam::config>(yaml_node);
+// }
