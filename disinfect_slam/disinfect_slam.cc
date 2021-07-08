90407a60366c6ce39d11708451185fdd0a6577b9
yuzhou42 <yu.nico.zhou@gmail.com>
2021-07-07 19:14:59 -0500

    change openvslam to orbslam3

--- disinfect_slam/disinfect_slam.cc
+++ disinfect_slam/disinfect_slam.cc
@@ -1,36 +1,44 @@
 #include "disinfect_slam/disinfect_slam.h"
 
-DISINFSystem::DISINFSystem(std::string camera_config_path, std::string vocab_path,
-                           std::string seg_model_path, bool rendering_flag) {
-  std::shared_ptr<openvslam::config> cfg = GetAndSetConfig(camera_config_path);
-  SLAM_ = std::make_shared<SLAMSystem>(cfg, vocab_path);
-//   SEG_ = std::make_shared<inference_engine>(seg_model_path);
-  TSDF_ = std::make_shared<TSDFSystem>(0.01, 0.06, 4, GetIntrinsicsFromFile(camera_config_path),
-                                       GetExtrinsicsFromFile(camera_config_path));
+DISINFSystem::DISINFSystem(std::string camera_config_path,
+                           std::string vocab_path,
+                           std::string seg_model_path,
+                           const ORB_SLAM3::System::eSensor sensor, 
+                           bool pangolin_view)
+{
+    SLAM_ = std::make_shared<ORB_SLAM3::System>(
+        vocab_path, camera_config_path, sensor, pangolin_view);
 
-  depthmap_factor_ = GetDepthFactorFromFile(camera_config_path);
+    //   SEG_ = std::make_shared<inference_engine>(seg_model_path);
+    TSDF_ = std::make_shared<TSDFSystem>(0.05,
+                                         0.2,
+                                         4,
+                                         GetIntrinsicsFromFile(camera_config_path),
+                                         GetExtrinsicsFromFile(camera_config_path));
 
-  SLAM_->startup();
+    depthmap_factor_ = GetDepthFactorFromFile(camera_config_path);
+    camera_pose_manager_ = std::make_shared<pose_manager>();
 
-  camera_pose_manager_ = std::make_shared<pose_manager>();
-
-  if (rendering_flag) {
-    RENDERER_ = std::make_shared<ImageRenderer>("tsdf", SLAM_, TSDF_, camera_config_path);
-  }
+    // if (rendering_flag) {
+    //   RENDERER_ = std::make_shared<ImageRenderer>("tsdf", SLAM_, TSDF_, camera_config_path);
+    // }
 }
 
-DISINFSystem::~DISINFSystem() { SLAM_->shutdown(); }
+DISINFSystem::~DISINFSystem() {  }
 
-void DISINFSystem::run() { RENDERER_->Run(); }
+// void DISINFSystem::run() { RENDERER_->Run(); }
 
-void DISINFSystem::feed_rgbd_frame(const cv::Mat& img_rgb, const cv::Mat& img_depth,
-                                   int64_t timestamp, const cv::Mat& mask) {
-  cv::Mat my_img_rgb, my_img_depth, my_mask;  // local Mat that will be modified
-  const SE3<float> posecam_P_world = camera_pose_manager_->query_pose(timestamp);
-  cv::resize(img_rgb, my_img_rgb, cv::Size(), .5, .5);
-  cv::resize(img_depth, my_img_depth, cv::Size(), .5, .5);
-  my_img_depth.convertTo(my_img_depth, CV_32FC1,
-                         1. / depthmap_factor_);  // depth scale
+void DISINFSystem::feed_rgbd_frame(const cv::Mat& img_rgb,
+                                   const cv::Mat& img_depth,
+                                   int64_t timestamp,
+                                   const cv::Mat& mask)
+{
+    cv::Mat my_img_rgb, my_img_depth, my_mask; // local Mat that will be modified
+    const SE3<float> posecam_P_world = camera_pose_manager_->query_pose(timestamp);
+    cv::resize(img_rgb, my_img_rgb, cv::Size(), .5, .5);
+    cv::resize(img_depth, my_img_depth, cv::Size(), .5, .5);
+    my_img_depth.convertTo(my_img_depth, CV_32FC1,
+                           1. / depthmap_factor_); // depth scale
     if (!mask.empty())
     {
         // std::cout<<"mask is not empty!"<<std::endl;
@@ -38,7 +46,7 @@ void DISINFSystem::feed_rgbd_frame(const cv::Mat& img_rgb, const cv::Mat& img_de
         cv::Size s   = my_img_depth.size();
         int num_rows = s.height;
         int num_cols = s.width;
-        int cnt = 0;
+        int cnt      = 0;
         for (unsigned int i = 0; i < num_rows; ++i)
         {
             for (unsigned int j = 0; j < num_cols; ++j)
@@ -47,31 +55,54 @@ void DISINFSystem::feed_rgbd_frame(const cv::Mat& img_rgb, const cv::Mat& img_de
                 {
                     my_img_depth.at<float>(i, j) = 0.0;
                     cnt++;
-
                 }
             }
         }
         // std::cout<<"mask count: "<<cnt<<std::endl;
         // cv::imshow("mask_depth", my_img_depth);
         // cv::waitKey(1);
+    }
+    //   std::vector<cv::Mat> prob_map = SEG_->infer_one(my_img_rgb, false);
+    TSDF_->Integrate(posecam_P_world, my_img_rgb, my_img_depth);
+}
+
+void DISINFSystem::feed_stereo(const cv::Mat& img_left,
+                                   const cv::Mat& img_right,
+                                   double timestamp)
+{
+    cv::Mat Tcw(4, 4, CV_32FC1);
+    if(!Tcw.empty()){
+        Eigen::Matrix<float, 4, 4> eigenT;
+        cv::cv2eigen(Tcw, eigenT);
+        const SE3<float> posecam_P_world(eigenT);
+        camera_pose_manager_->register_valid_pose(timestamp*1e3, posecam_P_world); //s to ms
 
     }
-//   std::vector<cv::Mat> prob_map = SEG_->infer_one(my_img_rgb, false);
-  TSDF_->Integrate(posecam_P_world, my_img_rgb, my_img_depth);
 }
 
-void DISINFSystem::feed_stereo_frame(const cv::Mat& img_left, const cv::Mat& img_right,
-                                     int64_t timestamp, const cv::Mat& mask) {
-  const pose_valid_tuple m =
-      SLAM_->feed_stereo_images_w_feedback(img_left, img_right, timestamp / 1e3, mask);
-  const SE3<float> posecam_P_world(m.first.cast<float>().eval());
-  if (m.second) camera_pose_manager_->register_valid_pose(timestamp, posecam_P_world);
+void DISINFSystem::feed_stereo_IMU(const cv::Mat& img_left,
+                                   const cv::Mat& img_right,
+                                   double timestamp,
+                                   const std::vector<ORB_SLAM3::IMU::Point>& vImuMeas)
+{
+    cv::Mat Tcw(4, 4, CV_32FC1);
+    Tcw = SLAM_->TrackStereo(img_left, img_right, timestamp, vImuMeas);
+    if(!Tcw.empty()){
+        Eigen::Matrix<float, 4, 4> eigenT;
+        cv::cv2eigen(Tcw, eigenT);
+        const SE3<float> posecam_P_world(eigenT);
+        camera_pose_manager_->register_valid_pose(timestamp*1e3, posecam_P_world); //s to ms
+
+    }
+   
 }
 
-SE3<float> DISINFSystem::query_camera_pose(const int64_t timestamp) {
-  return this->camera_pose_manager_->query_pose(timestamp);
+SE3<float> DISINFSystem::query_camera_pose(const int64_t timestamp)
+{
+    return this->camera_pose_manager_->query_pose(timestamp);
 }
 
-std::vector<VoxelSpatialTSDF> DISINFSystem::query_tsdf(const BoundingCube<float>& volumn) {
-  return TSDF_->Query(volumn);
+std::vector<VoxelSpatialTSDF> DISINFSystem::query_tsdf(const BoundingCube<float>& volumn)
+{
+    return TSDF_->Query(volumn);
 }
--- disinfect_slam/disinfect_slam.h
+++ disinfect_slam/disinfect_slam.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <openvslam/publish/map_publisher.h>
-#include <openvslam/system.h>
+// #include <openvslam/publish/map_publisher.h>
+// #include <openvslam/system.h>
 #include <spdlog/spdlog.h>
 #include <yaml-cpp/yaml.h>
 
@@ -14,40 +14,49 @@
 #include <utility>
 #include <vector>
 
-#include "modules/renderer_module.h"
-#include "modules/slam_module.h"
+// #include "modules/renderer_module.h"
+// #include "modules/slam_module.h"
 #include "modules/tsdf_module.h"
 #include "segmentation/inference.h"
 #include "utils/config_reader.hpp"
 #include "utils/cuda/errors.cuh"
-#include "utils/gl/renderer_base.h"
+// #include "utils/gl/renderer_base.h"
 #include "utils/rotation_math/pose_manager.h"
 #include "utils/time.hpp"
+#include "System.h"
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/opencv.hpp>
+
+#define NANO2SEC(ns) (double(ns) / 1e9)
 
 class DISINFSystem {
  public:
-  DISINFSystem(std::string camera_config_path, std::string vocab_path, std::string seg_model_path,
-               bool rendering_flag);
-
+  DISINFSystem(std::string camera_config_path, std::string vocab_path, std::string seg_model_path, 
+                const ORB_SLAM3::System::eSensor sensor, bool pangolin_view = false);
   ~DISINFSystem();
 
   void feed_rgbd_frame(const cv::Mat& img_rgb, const cv::Mat& img_depth, int64_t timestamp, const cv::Mat& mask = cv::Mat{});
 
-  void feed_stereo_frame(const cv::Mat& img_left, const cv::Mat& img_right, int64_t timestamp, const cv::Mat& mask = cv::Mat{});
+  void feed_stereo(const cv::Mat& img_left, const cv::Mat& img_right, double timestamp);
+
+  void feed_stereo_IMU(const cv::Mat& img_left, const cv::Mat& img_right, double timestamp, const std::vector<ORB_SLAM3::IMU::Point>& vImuMeas);
 
   std::vector<VoxelSpatialTSDF> query_tsdf(const BoundingCube<float>& volumn);
 
   SE3<float> query_camera_pose(const int64_t timestamp);
 
   void run();
+  std::shared_ptr<ORB_SLAM3::System> SLAM_;
+  std::shared_ptr<TSDFSystem> TSDF_;
+  std::shared_ptr<pose_manager> camera_pose_manager_;
+
+
 
  private:
-  std::shared_ptr<SLAMSystem> SLAM_;
   // std::shared_ptr<inference_engine> SEG_;
-  std::shared_ptr<TSDFSystem> TSDF_;
-  std::shared_ptr<ImageRenderer> RENDERER_;
+  // std::shared_ptr<ImageRenderer> RENDERER_;
 
-  std::shared_ptr<pose_manager> camera_pose_manager_;
 
   float depthmap_factor_;
 };
